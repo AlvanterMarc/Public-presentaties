350edd3bfa2a1bab76cad42d6ac9bf5a6560ddb3
Marc Hassing <marc.hassing@ictu.nl>
2026-02-23 12:54:29 +0100

    Presenatie toegevoegd

---
+++ OpenRewrite_2026/code/Oldcode/src/main/oldcode/com/alvant/oldcode/OldcodeApplication.groovy
@@ -0,0 +1,14 @@
+package com.alvant.oldcode
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OldcodeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(OldcodeApplication, args)
+        System.out.println("Hello world, Alvant!")
+    }
+
+}
---
+++ OpenRewrite_2026/code/Oldcode/src/test/groovy/com/alvant/oldcode/OldcodeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.alvant.oldcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OldcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
